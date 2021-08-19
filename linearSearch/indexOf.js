616d00f34b698ae966d59929c9b9fb157ff34b02
Dylan Harville <dr.dyloh@gmail.com>
2021-08-19 01:38:24 -0600

    linear search complete and working as expected

--- linearSearch/indexOf.js
+++ linearSearch/indexOf.js
@@ -10,3 +10,13 @@ function indexOf(isMatch, elements) {
 }
 
 module.exports = indexOf;
+
+/**isMatch is a function that takes the current element, 
+current index, and original array as parameters. 
+It returns true when a matching element is found. */
+
+/**algorithm goes through all of the elements in the array 
+and calls isMatch(), passing in the element, index, and array. 
+The variable index keeps track of where it is in the array. 
+If isMatch() returns true, it will return the current value for index. 
+But in cases where isMatch() always returns false, -1 is returned**/
